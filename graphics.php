110829dba08dab211bf48048d4d93bae2b870469
Deedee Chiang <DDChiang@users.noreply.github.com>
2014-10-29 21:34:52 -0700

    init commit

---
+++ graphics.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/home-custom.css">
+	<link rel="stylesheet" type="text/css" href="css/web-app-custom.css">
+	<link rel="stylesheet" type="text/css" href="css/graphics-custom.css">
+	<title>DDCHIANG</title>
+	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="script.js"></script>
+    <?php include('header.php'); ?>
+  </head>
+  
+  <body>
+    <div class="content-wrapper">
+	  
+	    <div class="box-50"><img src="graphics/Benjamin-Britts-long.png"></div>
+		<div class="box-50"><img src="graphics/Benjamin-Britts-Teaser.png"></div>
+		<div class="box-50"> <img src="graphics/home-and-heart.png"></div>
+		<div class="box-50"><img src="graphics/jet-black.png"></div>
+		
+		<div class="box-50"><img src="prev-pics/odysey-timeline.png"></div>
+		
+		
+		
+		<div class="box-50"><img src="graphics/glow-in-dark-sandal.png"></div>
+		<div class="box-50"><img src="graphics/platform-sandal-anat.png"></div>
+		<div class="box-50"><img src="graphics/black-sandals.png"></div>
+		<div class="box-50"><img src="graphics/blue-sandal.png"></div>
+		<div class="box-50"><img src="graphics/nucleus-grey-gradient.png"></div>
+		<div class="box-50"><img src="graphics/nucleus-purple-gradient.png"></div>
+		<div class="box-50"><img src="graphics/purses.png"></div>
+		<div class="box-50"><img src="graphics/purses-large.png"></div>
+		
+		<div class="box-50"><img src="graphics/wicked-icons.png"></div>
+		
+		<div class="box-50"> <img src="prev-pics/stgram-logos.png"></div>
+		  <div class="box-50"><img src="graphics/strawby-black.png"></div>
+		   
+	  	</div>
+	</div>
+  </body>
+</html>  
---
+++ photos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/home-custom.css">
+	<link rel="stylesheet" type="text/css" href="css/web-app-custom.css">
+	<link rel="stylesheet" type="text/css" href="css/graphics-custom.css">
+	<title>DDCHIANG</title>
+	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="script.js"></script>
+    <?php include('header.php'); ?>
+  </head>
+  
+  <body>
+    <div class="content-wrapper">
+	  
+	    <div class="box-50"><img src="photos/sand-spikes.jpg"></div>
+		 <div class="box-50"><img src="photos/sand-spikes-ready.jpg"></div>
+		 <div class="box-50"><img src="photos/water-dimps.jpg"></div>
+		 <div class="box-50"><img src="photos/light-rings.jpg"></div>
+		 <div class="box-50"><img src="photos/whish.jpg"></div>
+		 <div class="box-50"><img src="photos/brush.jpg"></div>
+		 <div class="box-50"><img src="photos/fung.jpg"></div>
+		 <div class="box-50"><img src="photos/bluegreen.jpg"></div>
+		 <div class="box-50"><img src="photos/paper-red.jpg"></div>
+		 <div class="box-50"><img src="photos/paper-dots.jpg"></div>
+	</div>
+  </body>
+</html>  
