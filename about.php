0458045c114e452beb30a9fed32d26e549377e6c
Deedee Chiang <DDChiang@users.noreply.github.com>
2014-11-12 23:07:31 -0800

    change google font request to https

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="css/graphics-custom.css">
 	<link rel="stylesheet" type="text/css" href="css/about-custom.css">
 	<title>DDCHIANG</title>
-	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="script.js"></script>
